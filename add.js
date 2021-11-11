81bc50d232de91b68848fb56cd7e116a6b0ee4d3
Keyuan01 <zhaokeyuan72@gmail.com>
2021-11-10 18:55:31 -0600

    add_user.js prueba

--- add.js
+++ add.js
@@ -1,3 +1,4 @@
+/*
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";//Importacion firebase
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";//impotacion modulo realtimedatabse
 import { getAuth, signOut, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";//importacion firebase auth
@@ -17,28 +18,70 @@ const app = initializeApp(firebaseConfig);
 
 //Revisar si esta ingresado
 checkauth();
-
+ */
 
 $("#logoutButton").on("click", function(){
   singout();
 });
 
 $("#btn_add_admin").on("click", function(){
-    var username=$("#username").val();
-    var email=$("#email").val();
-    var pass=$("#pass").val();
-    var confirm_pass=$("#confirm_pass").val();
-    if(pass != confirm_pass){
-        alert("Contraseñas distintas");
-    }
-    else{
-        register_admin(username, email, pass);
-    }
+  var username=$("#username").val();
+  var email=$("#email").val();
+  var pass=$("#pass").val();
+  var confirm_pass=$("#confirm_pass").val();
+  if(pass != confirm_pass){
+      alert("Contraseñas distintas");
+  }
+  else{
+      register_admin(username, email, pass);
+  }
 });
 
 $("#btn_add_user").on("click", function(){
-  
+  var username=$("#username").val();
+  var email=$("#email").val();
+  var phone=$('#phone').val();
+  var address=$('#phone').val();
+  var age=$('#age').val();
+  alert(age);
+  var pronoun=$('pro').val();
+  var job=$('#job').val();
+  var jobdesc=$('#jobdesc').val();
 
+  var datejobX = "";
+
+  if($('#lunes').proc('checked')){
+      datejobX = datejob + "L ";
+  }
+  if($('#martes').proc('checked')){
+      datejobX = datejob + "M ";
+  }
+  if($('#miercoles').proc('checked')){
+      datejobX = datejob + "X ";
+  }
+  if($('#jueves').proc('checked')){
+      datejobX = datejob + "J ";
+  }
+  if($('#viernes').proc('checked')){
+      datejobX = datejob + "V ";
+  }
+  if($('#sabado').proc('checked')){
+      datejobX = datejob + "S ";
+  }
+  if($('#domingo').proc('checked')){
+      datejobX = datejob + "D ";
+  }
+  datejob = datejobX.toString();
+  alert("Los días son: " + datejob);
+
+  var pass=$("#pass").val();
+  var confirm_pass=$("#confirm_pass").val();
+  if(pass != confirm_pass){
+      alert("Contraseñas distintas");
+  }
+  else{
+      register_user(username, email, phone, address, age, pronoun, job, jobdesc, pass);
+  }
 });
 
 
@@ -73,7 +116,6 @@ function checkauth(){
 function register_admin(username, email, password){
     const auth = getAuth();
     const db = getDatabase();
-    //const user = auth.currentUser;
     //Registrar administrador en database
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -94,6 +136,34 @@ function register_admin(username, email, password){
   });
 }
 
-function register_user(){
-
+function register_user(username, email, phone, address, age, pronoun, job, jobdesc, datejob, password){
+  const auth = getAuth();
+  const db = getDatabase();
+  //Registrar usuario en database
+  createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          // Signed in
+          const uid = userCredential.user.uid;
+          //alert("UID es: " + uid);
+          //Escribirlo en la base de datos
+          set(ref(db, 'Users/' + uid),{
+              name: username,
+              role: "user",
+              phone: phone,
+              pronoun: pronoun,
+              address: address,
+              age: age,
+              T1: {
+                datejob: datejob,
+                jobtitle: job,
+                jobdesc: jobdesc
+              }
+          })
+          alert("Usuario registrado exitosamente");
+      })
+      .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          alert("Error, " + errorMessage);
+      });
 }
