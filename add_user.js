980b9f5ace0be80323e6e3771cfe4792148695aa
Keyuan01 <zhaokeyuan72@gmail.com>
2021-11-10 23:15:30 -0600

    test final add_user.js

--- add_user.js
+++ add_user.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.2.0/
 import { getAuth, signOut, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";//importacion firebase auth
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
-  authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
-  databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
-  projectId: "banco-de-tiempo-77b4e",
-  storageBucket: "banco-de-tiempo-77b4e.appspot.com",
-  messagingSenderId: "399972830225",
-  appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
-  measurementId: "G-J7Y6ZZT5YJ"
+    apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
+    authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
+    databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
+    projectId: "banco-de-tiempo-77b4e",
+    storageBucket: "banco-de-tiempo-77b4e.appspot.com",
+    messagingSenderId: "399972830225",
+    appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
+    measurementId: "G-J7Y6ZZT5YJ"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,40 +19,39 @@ const app = initializeApp(firebaseConfig);
 checkauth();
 
 $("#logoutButton").on("click", function(){
-  singout();
+    singout();
 });
 
 $("#btn_add_user").on("click", function(){
-  var username=$("#username").val();
-  var email=$("#email").val();
-  var phone=$('#phone').val();
-  var address=$('#address').val();
-  var age=$('#age').val();
-  var pronoun=$('#pronoun').val();
-  var job=$('#job').val();
-  var jobdesc=$('#jobdesc').val();
+    var username=$("#username").val();
+    var email=$("#email").val();
+    var phone=$('#phone').val();
+    var address=$('#address').val();
+    var age=$('#age').val();
+    var pronoun=$('#pronoun').val();
+    var job=$('#job').val();
+    var jobdesc=$('#jobdesc').val();
 
-  var datejob = "";
+    var datejob = "";
 
-  if($('#lunes').is(':checked')){ datejob = datejob + "L "; }
-  if($('#martes').is(':checked')){ datejob = datejob + "M "; }
-  if($('#miercoles').is(':checked')){ datejob = datejob + "X "; }
-  if($('#jueves').is(':checked')){ datejob = datejob + "J "; }
-  if($('#viernes').is(':checked')){ datejob = datejob + "V "; }
-  if($('#sabado').is(':checked')){ datejob = datejob + "S "; }
-  if($('#domingo').is(':checked')){ datejob = datejob + "D "; }
+    if($('#lunes').is(':checked')){ datejob = datejob + "L "; }
+    if($('#martes').is(':checked')){ datejob = datejob + "M "; }
+    if($('#miercoles').is(':checked')){ datejob = datejob + "X "; }
+    if($('#jueves').is(':checked')){ datejob = datejob + "J "; }
+    if($('#viernes').is(':checked')){ datejob = datejob + "V "; }
+    if($('#sabado').is(':checked')){ datejob = datejob + "S "; }
+    if($('#domingo').is(':checked')){ datejob = datejob + "D "; }
 
-  var pass=$("#pass").val();
-  var confirm_pass=$("#confirm_pass").val();
-  if(pass != confirm_pass){
-      alert("Contraseñas distintas");
-  }
-  else{
-      //register_user(username, email, phone, address, age, pronoun, job, jobdesc, pass);
-      register_user(username, email, pass, phone, address, age, pronoun, datejob);
-  }
+    var pass=$("#pass").val();
+    var confirm_pass=$("#confirm_pass").val();
+    if(pass != confirm_pass){
+        alert("Contraseñas distintas");
+    }
+    else{
+        //register_user(username, email, phone, address, age, pronoun, job, jobdesc, pass);
+        register_user(username, email, pass, phone, address, age, pronoun, job, jobdesc, datejob);
+    }
 });
-
 
 //Función para salir de la sesión
 function singout(){
@@ -81,7 +80,7 @@ function checkauth(){
     });
 }
 
-function register_user(username, email, password, phone, address, age, pronoun, datejob){
+function register_user(username, email, password, phone, address, age, pronoun, job, jobdesc, datejob){
     const auth = getAuth();
     const db = getDatabase();
     //Registrar administrador en database
@@ -99,8 +98,16 @@ function register_user(username, email, password, phone, address, age, pronoun, 
             address: address,
             age: age,
             pronoun: pronoun,
-            T1: {
-                datejob: datejob
+            status: "No verificado",
+            rating: 0,
+            totjobs: 0,
+            totrating: 0,
+            jobs: {
+                T1: {
+                    jobtitle: job,
+                    datejob: datejob,
+                    jobdesc: jobdesc
+                }
             }
         })
         alert("Usuario registrado exitosamente");
@@ -111,37 +118,3 @@ function register_user(username, email, password, phone, address, age, pronoun, 
         alert(errorMessage);
   });
 }
-
-/*
-function register_user(username, email, phone, address, age, pronoun, job, jobdesc, datejob, password){
-  const auth = getAuth();
-  const db = getDatabase();
-  //Registrar usuario en database
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const uid = userCredential.user.uid;
-      //alert("UID es: " + uid);
-      //Escribirlo en la base de datos con el rol de administrador
-      set(ref(db, 'Users/' + uid),{
-        name: username,
-        role: "user",
-        phone: phone,
-        pronoun: pronoun,
-        address: address,
-        age: age,
-        T1: {
-          datejob: datejob,
-          jobtitle: job,
-          jobdesc: jobdesc
-        }
-      })
-      alert("Usuario registrado exitosamente");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-}
-*/
