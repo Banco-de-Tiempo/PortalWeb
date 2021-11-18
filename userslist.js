a64fbe386afcc98e64dd90d6be4b78fc8bbe27bc
Alan <aeaquinorosas@hotmail.com>
2021-11-17 21:36:31 -0600

    Align elements with some fixes

---
+++ userslist.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";//Importacion firebase
+import { getDatabase, ref, set, onValue} from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";//impotacion modulo realtimedatabse
+import { getAuth ,createUserWithEmailAndPassword ,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";//importacion firebase auth
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
+  authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
+  databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
+  projectId: "banco-de-tiempo-77b4e",
+  storageBucket: "banco-de-tiempo-77b4e.appspot.com",
+  messagingSenderId: "399972830225",
+  appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
+  measurementId: "G-J7Y6ZZT5YJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getDatabase();
+
+const starCountRef = ref(db,'Users');
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+
+});
+// version funcional
