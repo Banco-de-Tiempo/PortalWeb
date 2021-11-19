3c130c87b8dece8db606aed9c4b70bb3b8b571c3
Keyuan01 <zhaokeyuan72@gmail.com>
2021-11-19 16:07:14 -0600

    test consulta de usuarios por estatus

--- userslist.js
+++ userslist.js
@@ -1,25 +1,128 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";//Importacion firebase
-import { getDatabase, ref, set, onValue} from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";//impotacion modulo realtimedatabse
-import { getAuth ,createUserWithEmailAndPassword ,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";//importacion firebase auth
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";//impotacion modulo realtimedatabse
+import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";//importacion firebase auth
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
-  authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
-  databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
-  projectId: "banco-de-tiempo-77b4e",
-  storageBucket: "banco-de-tiempo-77b4e.appspot.com",
-  messagingSenderId: "399972830225",
-  appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
-  measurementId: "G-J7Y6ZZT5YJ"
+    apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
+    authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
+    databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
+    projectId: "banco-de-tiempo-77b4e",
+    storageBucket: "banco-de-tiempo-77b4e.appspot.com",
+    messagingSenderId: "399972830225",
+    appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
+    measurementId: "G-J7Y6ZZT5YJ"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getDatabase();
 
-const starCountRef = ref(db,'Users');
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
+//Revisar si esta ingresado
+checkauth();
 
+$("#logoutButton").on("click", function(){
+    singout();
 });
-// version funcional
+
+$("#btn_consultar").on("click", function(){
+    var status=$('#select')
+    consultar_status(status);
+});
+
+//Preparar las condiciones para mostrar los datos en la tabla
+const tabla = document.querySelector('#list_consulta_estatus tbody');
+const row = document.createElement('tr');
+
+//Funcion de consultar usuario
+function consultar_status(status){
+    const db = getDatabase();
+    const starCountRef = ref(db, 'Users/');
+
+    var encontrado = new Boolean(false);
+    
+    //Vaciar la tabla
+    row.innerHTML = "";
+    //Iniciar la busqueda por nombre
+    onValue(starCountRef, (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            const childData = childSnapshot.val();
+            //alert(childData.name);
+            //Buscar el usuario con el nombre y el número telefono
+            if (status == childData.status){
+                const uid = childSnapshot.key;
+                //alert("Encontrado");
+                //alert(childData.name);
+                encontrado = true;
+                try {
+                    var job1 = childData.jobs.T1.jobtitle;
+                } catch {
+                    var job1 = "No existe";
+                }
+                try {
+                    var job2 = childData.jobs.T2.jobtitle;
+                } catch {
+                    var job2 = "No existe";
+                }
+                try {
+                    var job3 = childData.jobs.T3.jobtitle;
+                } catch {
+                    var job3 = "No existe";
+                }
+                //Convertir número de trabajo a integer
+                //const tot_jobs = parseInt(childData.jobs.number);
+                //var jobs_title = "";
+
+                //Mensaje de no existe
+                //var desc = "No existe";
+
+                //Concatenar los nombre de trabajo a un variable
+                row.innerHTML += `
+                    <td>${childSnapshot.key}</td>
+                    <td>${childData.name}</td>
+                    <td>${childData.age}</td>
+                    <td>${childData.phone}</td>
+                    <td>${job1}</td>
+                    <td>${job2}</td>
+                    <td>${job3}</td>
+                    <td>${childData.balance}</td>
+                    <td>${childData.rating}</td>
+                    <td>${childData.status}</td>
+                    <td>${childData.totjobs}</td>
+                `;
+                //Mostrarlo en la tabla
+                tabla.appendChild(row);
+            }
+        });
+        if (encontrado == false) {
+            alert("Usuario no encontrado o no existe en base de datos");
+        }
+    }, {
+        onlyOnce: true
+    });
+}
+
+
+//Función para salir de la sesión
+function singout(){
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        alert("Sesión cerrada correctamente");
+        window.location.replace("index.html");
+    }).catch((error) => {
+        alert("Error cerrando la sesión");
+    });
+}
+
+//Función de checar si está ingresado
+function checkauth(){
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        //alert("Bienvenido de nuevo");
+        } else {
+        // User is signed out
+        alert("Porfavor ingrese sesión");
+        window.location.replace("iniciarsesion.html");
+        }
+    });
+}
