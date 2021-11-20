b584e09e9100f154ed86ef50eec1c7b3644230e4
Keyuan01 <zhaokeyuan72@gmail.com>
2021-11-20 02:26:07 -0600

    test consulta de usuarios por estatus

--- userslist.js
+++ userslist.js
@@ -30,7 +30,6 @@ $("#btn_consultar").on("click", function(){
 
 //Preparar las condiciones para mostrar los datos en la tabla
 const tabla = document.querySelector('#list_consulta_estatus tbody');
-const row = document.createElement('tr');
 
 //Funcion de consultar usuario
 function consultar_status(status){
@@ -40,7 +39,7 @@ function consultar_status(status){
     var encontrado = new Boolean(false);
     
     //Vaciar la tabla
-    row.innerHTML = "";
+    tabla.innerHTML = "";
     //Iniciar la busqueda por nombre
     onValue(starCountRef, (snapshot) => {
         snapshot.forEach((childSnapshot) => {
@@ -67,24 +66,24 @@ function consultar_status(status){
                 } catch {
                     var job3 = "No existe";
                 }
-
-                row.innerHTML += `
-                    <td>${childSnapshot.key}</td>
-                    <td>${childData.name}</td>
-                    <td>${childData.age}</td>
-                    <td>${childData.phone}</td>
-                    <td>${job1}</td>
-                    <td>${job2}</td>
-                    <td>${job3}</td>
-                    <td>${childData.balance}</td>
-                    <td>${childData.rating}</td>
-                    <td>${childData.status}</td>
-                    <td>${childData.totjobs}</td>
+                
+                var row =  `<tr>
+                                <td>${childSnapshot.key}</td>
+                                <td>${childData.name}</td>
+                                <td>${childData.age}</td>
+                                <td>${childData.phone}</td>
+                                <td>${job1}</td>
+                                <td>${job2}</td>
+                                <td>${job3}</td>
+                                <td>${childData.balance}</td>
+                                <td>${childData.rating}</td>
+                                <td>${childData.status}</td>
+                                <td>${childData.totjobs}</td>
+                            </tr>
                 `;
                 
                 //Mostrarlo en la tabla
-                tabla.appendChild(row);
-
+                tabla.innerHTML += row;
             }
         });
         if (encontrado == false) {
